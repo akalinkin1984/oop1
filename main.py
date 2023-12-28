d82133c6b58360c91c14d54684e3727c5d302a77
Alexandr Kalinkin <akalinkin84@yandex.ru>
2023-12-28 22:42:02 +0300

    commit3

--- main.py
+++ main.py
@@ -93,9 +93,6 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
